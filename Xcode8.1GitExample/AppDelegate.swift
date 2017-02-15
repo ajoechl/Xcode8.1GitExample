a8bb3cc277e3e825a03029c7d9f358072148cacc
Jöchl Andrew <andrew.joechl@pistenbully.com>
2017-02-15 15:40:06 -0500

    Initial Commit

---
+++ Xcode8.1GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8.1GitExample
+//
+//  Created by Andrew Joechl on 2/15/17.
+//  Copyright © 2017 Andrew Joechl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8.1GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8.1GitExample
+//
+//  Created by Andrew Joechl on 2/15/17.
+//  Copyright © 2017 Andrew Joechl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
